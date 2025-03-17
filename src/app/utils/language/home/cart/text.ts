be8a511a253b8d192d60c020c9df6a507a0147ba
wolfangherrera <andreyherrerac@icloud.com>
2025-03-16 20:48:12 -0500

    feat: add multilingual support for cart component text and update header structure

---
+++ src/app/utils/language/home/cart/text.ts
@@ -0,0 +1,28 @@
+export type typeCartText = {
+    mainText: string,
+    subtitleText: string
+    emptyCartText: string
+    buttonCart: string
+    mainTextCustomerDetails: string
+    subtextCustomerDetails: string
+};
+
+
+export const EN_cartText : typeCartText = {
+    mainText: 'YOUR CART',
+    subtitleText: 'ITEMS',
+    emptyCartText: 'NO ITEMS IN CART',
+    buttonCart: 'CHECKOUT',
+    mainTextCustomerDetails: 'CUSTOMER DETAILS',
+    subtextCustomerDetails: 'CUSTOMER INFO',
+};
+
+
+export const ES_cartText : typeCartText = {
+    mainText: 'TU CARRITO',
+    subtitleText: 'ARTÍCULOS',
+    emptyCartText: 'NO HAY ARTÍCULOS EN EL CARRITO',
+    buttonCart: 'PAGAR',
+    mainTextCustomerDetails: 'DETALLES DEL CLIENTE',
+    subtextCustomerDetails: 'INFORMACIÓN DEL CLIENTE',
+};
