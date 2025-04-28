061d6ffa527ca0096ee9056cdcc1bb1f5f3af658
wolfangherrera <andreyherrerac@icloud.com>
2025-04-27 23:54:27 -0500

    Hotfix: Text nowrap

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -55,7 +55,7 @@ export class HomePage implements OnInit, AfterViewInit{
   disableLoading() {
     setTimeout(() => {
       this.isLoading = false;
-      this.tabs.select('Account');
+      this.tabs.select('Home');
     }, 1000);
   }
 
