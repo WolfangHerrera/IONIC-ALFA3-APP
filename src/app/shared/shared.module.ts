a83d810c0c8f646abebe432912dfdd87b4bfcc01
wolfangherrera <andreyherrerac@icloud.com>
2025-05-02 20:14:19 -0500

    Feat: Create Header component and integrate into Home and Order pages

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,17 +2,20 @@ import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoadingComponent } from './loading/loading.component';
 import { IonicModule } from '@ionic/angular';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
     LoadingComponent,
+    HeaderComponent
   ],
   imports: [
     CommonModule,
     IonicModule
   ],
   exports: [
-    LoadingComponent
+    LoadingComponent,
+    HeaderComponent
   ],
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
